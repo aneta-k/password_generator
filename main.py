1ae11030447a20522bfae30a6fc9a561a77c6840
aneta-k <kusnierz.aneta@gmail.com>
2022-08-24 13:24:35 +0200

    made prompt for user about password lenght

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import string
 
 string_with_all_characters = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 
-
+password_lenght = int(input('How many characters do you want for your password: '))
 
