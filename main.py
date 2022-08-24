65c3c4b13a3a8aac663b06c0bda1bbac640adf0d
aneta-k <kusnierz.aneta@gmail.com>
2022-08-24 13:23:52 +0200

    made string with all possible characters

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from random import choice
+import string
+
+string_with_all_characters = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+
 
