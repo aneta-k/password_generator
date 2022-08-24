f7de3ade0a6d4d3f6003e1f933f76f313941ca64
aneta-k <kusnierz.aneta@gmail.com>
2022-08-24 14:30:12 +0200

    code clean-up

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from calendar import c
 from random import choice, randint
-from re import A
 import string
-from this import d
 
 new_password = ""
 password_lenght = int(input('How many characters do you want for your passwor, must be more than 4: ')) 
@@ -17,20 +14,15 @@ punctuation = string.punctuation
 
 a = randint(1, (password_lenght - 3))
 print(a)
-if 0 < a and a < password_lenght: 
-    b = randint(1, (password_lenght - a - 2))
-    print(b)
-    if 0 < a + b  and a + b < password_lenght:
-        c = randint(1, (password_lenght - a- b - 1))
-        print(c)
-        if 0 < a + b + c and a + b + c < password_lenght:
-            d = randint(1, (password_lenght - a - b - c))
-            print(f'koniec{d}')
-        else:
-            password_lenght = a + b + c
-            print(password_lenght)
-    else:
-        password_lenght = a
+
+b = randint(1, (password_lenght - a - 2))
+print(b)
+
+c = randint(1, (password_lenght - a- b - 1))
+print(c)
+
+d = password_lenght - a - b - c
+print(f'koniec{d}')
 
 # i = 0
 # while i <= password_lenght:
