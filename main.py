a87ebde0fdf21d717ef100d0a57b0cdfcf2b9319
ChrisIwanowski <bazinga1@gmail.com>
2022-08-24 13:49:16 +0200

    Podstawowy program gotowy

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import string
 
 string_with_all_characters = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 
+new_password = ""
 password_lenght = int(input('How many characters do you want for your password: '))
 
-# tu bylem 
+i = 0
+while i <= password_lenght:
+    new_password += choice(string_with_all_characters)
+    i += 1
+print(new_password)
